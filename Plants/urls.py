91750cbeea2a349f52355df116f5bb9b125714bd
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2024-04-22 22:24:33 -0500

    add URLs for plant list and detail views

--- Plants/urls.py
+++ Plants/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from Plants.views import plants
+from Plants.views import PlantListView, PlantDetailView  #, plants
 
 urlpatterns = [
-    path('', plants),
+    # path('', plants),
+    # path('plants-list/', PlantListView.as_view(), name='plants-list')
+    path('', PlantListView.as_view(), name='plants'),
+    path('<int:pk>/', PlantDetailView.as_view(), name='plant-detail'),
 ]
