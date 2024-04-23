90a1a9772be051614546d519ab94990895c24700
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2024-04-22 22:25:21 -0500

    creation of plant list and detail views

--- Plants/views.py
+++ Plants/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 # from django.shortcuts import HttpResponse
 
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from .models import Plant
+
 # Create your views here.
-def plants(request):
-    # return HttpResponse("<h1>This is plant index temp page</h1>")
-    return render(request, 'plants/index.html', {})
+# def plants(request):
+#     # return HttpResponse("<h1>This is plant index temp page</h1>")
+#     return render(request, 'plants/index.html', {})
+
+class PlantListView(ListView):
+    model = Plant
+    template_name = 'plants/plant_list.html'
+    context_object_name = 'plant_list'
+    ordering = ['name_common']
+    paginate_by = 10
+
+class PlantDetailView(DetailView):
+    model = Plant
+    template_name = 'plants/plant_detail.html'
+    context_object_name = 'plant'
