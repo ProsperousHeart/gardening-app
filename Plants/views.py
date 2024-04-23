38168a69655a05adddc6296fba84484fd10d6bdc
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2024-04-22 23:57:31 -0500

    comment out unneeded code

--- Plants/views.py
+++ Plants/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.shortcuts import HttpResponse
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.urls import reverse_lazy
 from .models import Plant
 
 # Create your views here.
+# class HomeView(TemplateView):
+#     template_name = 'Plants/index.html'
+
 # def plants(request):
 #     # return HttpResponse("<h1>This is plant index temp page</h1>")
 #     return render(request, 'plants/index.html', {})
@@ -21,3 +24,11 @@ class PlantDetailView(DetailView):
     model = Plant
     template_name = 'plants/plant_detail.html'
     context_object_name = 'plant'
+
+    # def get_object(self, queryset=None):
+    #     # Retrieve the object based on a specific condition
+    #     # For example, retrieving based on a slug field
+    #     print(self.kwargs)
+    #     plant_id = self.kwargs.get('pk')
+    #     obj = get_object_or_404(Plant, pk=plant_id)
+    #     return obj
