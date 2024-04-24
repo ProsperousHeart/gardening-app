c7cb76cc02631753b6903f263f690a7559757190
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2024-04-23 23:28:43 -0500

    addition of Nursery model

--- Plants/admin.py
+++ Plants/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Plant, PlantLink
+from .models import Plant, PlantLink, Nursery
+
+short_description = "Duplicate selected items"
 
 def duplicate_selected_plant(modeladmin, request, queryset):
     for obj in queryset:
@@ -7,7 +9,7 @@ def duplicate_selected_plant(modeladmin, request, queryset):
         obj.name_common = f"[COPY] {obj.name_common}"
         obj.save()
 
-duplicate_selected_plant.short_description = "Duplicate selected items"
+duplicate_selected_plant.short_description = short_description
 
 class PlantAdmin(admin.ModelAdmin):
     actions = [duplicate_selected_plant]
@@ -18,12 +20,24 @@ def duplicate_selected_link(modeladmin, request, queryset):
         obj.title = f"[COPY] {obj.title}"
         obj.save()
 
-duplicate_selected_link.short_description = "Duplicate selected items"
+duplicate_selected_link.short_description = short_description
 
 class PlantLinkAdmin(admin.ModelAdmin):
+    actions = [duplicate_selected_link]
+
+def duplicate_selected_nursery(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.pk = None  # Set primary key to None to create a new object
+        obj.title = f"[COPY] {obj.title}"
+        obj.save()
+
+duplicate_selected_nursery.short_description = short_description
+
+class NurseryAdmin(admin.ModelAdmin):
     actions = [duplicate_selected_link]
 
 # Register your models here.
 
 admin.site.register(Plant, PlantAdmin)
 admin.site.register(PlantLink, PlantLinkAdmin)
+admin.site.register(Nursery, NurseryAdmin)
